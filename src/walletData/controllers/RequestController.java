6257652b732df73ea650da8678612c5afd14bd45
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2018-10-08 20:48:04 +0530

    Completed GUI of login, admin request and hover action

--- src/walletData/controllers/RequestController.java
+++ src/walletData/controllers/RequestController.java
@@ -1,8 +1,8 @@
 package walletData.controllers;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import walletData.Query.Execute;
 import walletData.Scenes.LayOut;
 import walletData.dbs.DBConnect;
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 public class RequestController extends LayOut{
 
     @FXML
-    private TextField amount;
+    private JFXTextField amount;
 
     @FXML
     private Label status;

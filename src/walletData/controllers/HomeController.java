76b7755266893ba7d1c79af6d88d355b862bfbbe
HOD101s <manasbass.99@gmail.com>
2018-10-18 02:30:48 +0530

    usd currency toggle

--- src/walletData/controllers/HomeController.java
+++ src/walletData/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package walletData.controllers;
 
+import com.jfoenix.controls.JFXToggleButton;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.BooleanProperty;
@@ -19,6 +20,7 @@ import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
 
 public class HomeController extends LayOut {
 
@@ -30,6 +32,9 @@ public class HomeController extends LayOut {
 
     @FXML
     private Label balance;
+
+    @FXML
+    private Label currency;
 
     @FXML
     private TableView<HomeTable> hometable;
@@ -52,50 +57,34 @@ public class HomeController extends LayOut {
     @FXML
     private TableColumn<HomeTable, Integer> amount;
 
+    @FXML
+    private JFXToggleButton usdtoggle;
+
     ObservableList<HomeTable> data;
+
+    int mykey;
+    int bal ;
 
     @FXML
     private void initialize() {
         try {
             homeUser.setText(LoginController.loggeduser);
-            int mykey;
-            String bal;
             mykey = getkey();                                                 //gets public key
             String key = Integer.toString(mykey);
-            publicKey.setText(key);
-            bal = getbal(mykey);                                                 //gets user balance
-            balance.setText(bal);
+            publicKey.setText(key);                                                //gets user balance
+            bal = Integer.parseInt(getbal(mykey));
+            balance.setText(String.valueOf(bal));
             transID.setCellValueFactory(new PropertyValueFactory<HomeTable, String>("transID"));
             date.setCellValueFactory(new PropertyValueFactory<HomeTable, String>("date"));
             time.setCellValueFactory(new PropertyValueFactory<HomeTable, String>("time"));
             sender.setCellValueFactory(new PropertyValueFactory<HomeTable, Integer>("sender"));
-//            new PropertyValueFactory<HomeTable, Integer>("sender")
             receiver.setCellValueFactory(new PropertyValueFactory<HomeTable, Integer>("receiver"));
             amount.setCellValueFactory(new PropertyValueFactory<HomeTable, Integer>("amount"));
-            buildData(mykey);
+            buildData();
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-    }
-
-    private void buildData(int key) throws SQLException {
-        data = FXCollections.observableArrayList();
-        PreparedStatement build = DBConnect.getConn().prepareStatement(Execute.homeTable);
-        build.setInt(1,key);
-        build.setInt(2,key);
-        ResultSet rs = build.executeQuery();
-        while (rs.next()) {
-            HomeTable ht = new HomeTable();
-            ht.transID.set(rs.getString("transID"));
-            ht.date.set(rs.getString("date"));
-            ht.time.set(rs.getString("time"));
-            ht.sender.set(rs.getInt("senderpub"));
-            ht.receiver.set(rs.getInt("receiverpub"));
-            ht.amount.set(rs.getInt("amount"));
-            data.add(ht);
-        }
-        hometable.setItems(data);
     }
 
     @FXML
@@ -111,6 +100,37 @@ public class HomeController extends LayOut {
     @FXML
     private void openRequest() throws IOException{
         myrequest();
+    }
+
+    @FXML
+    void usdConvert(ActionEvent event) throws SQLException {
+        if (usdtoggle.isSelected()) {
+            DecimalFormat df = new DecimalFormat("###.###");
+            balance.setText(String.valueOf(df.format(bal / 73.6)));
+            currency.setText("$");
+        } else {
+            balance.setText(String.valueOf(bal));
+            currency.setText("₹");
+        }
+    }
+
+    private void buildData() throws SQLException {
+        data = FXCollections.observableArrayList();
+        PreparedStatement build = DBConnect.getConn().prepareStatement(Execute.homeTable);
+        build.setInt(1,mykey);
+        build.setInt(2,mykey);
+        ResultSet rs = build.executeQuery();
+        while (rs.next()) {
+            HomeTable ht = new HomeTable();
+            ht.transID.set(rs.getString("transID"));
+            ht.date.set(rs.getString("date"));
+            ht.time.set(rs.getString("time"));
+            ht.sender.set(rs.getInt("senderpub"));
+            ht.receiver.set(rs.getInt("receiverpub"));
+            ht.amount.set(rs.getInt("amount"));
+            data.add(ht);
+        }
+        hometable.setItems(data);
     }
 
     public static int getkey() throws SQLException {
